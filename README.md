70308b8aa2500444e85b646815f0fcaadbf00754
indratrisnar <trisnaindra1998@gmail.com>
2024-05-15 09:32:40 +0700

    done

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Coffee Shop App  
 https://www.figma.com/community/file/1116708627748807811
+
+[Tutorial]()
+
+|                         Onboarding                         |                         Home                         |                         Detail                         |
+| :--------------------------------------------------------: | :--------------------------------------------------: | :----------------------------------------------------: |
+| <img src="screenshot/onboarding.png" style="height:450px"> | <img src="screenshot/home.png" style="height:450px"> | <img src="screenshot/detail.png" style="height:450px"> |
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:coffee_shop_ui/models/coffee.dart';
+import 'package:coffee_shop_ui/pages/dashboard_page.dart';
+import 'package:coffee_shop_ui/pages/detail_page.dart';
+import 'package:coffee_shop_ui/pages/onboard_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +14,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xffF9F9F9),
+        textTheme: GoogleFonts.soraTextTheme(),
       ),
+      home: const OnboardPage(),
+      routes: {
+        '/dashboard': (context) => const DashboardPage(),
+        '/detail': (context) {
+          Coffee coffee = ModalRoute.of(context)!.settings.arguments as Coffee;
+          return DetailPage(coffee: coffee);
+        },
+      },
     );
   }
 }
